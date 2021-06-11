8c266f829c91b8bb98eb2d4f8073d216b74dfbf1
Diederik Van Gogh <doctor_music@hotmail.com>
2021-06-11 13:57:23 +0200

    Add project files.

---
+++ ChefByStep.Xamarin/ChefByStep.Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChefByStep.Xamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChefByStep.Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ChefByStep.Xamarin/ChefByStep.Xamarin/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ChefByStep.Xamarin.Models;
+using ChefByStep.Xamarin.ViewModels;
+using ChefByStep.Xamarin.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChefByStep.Xamarin.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
