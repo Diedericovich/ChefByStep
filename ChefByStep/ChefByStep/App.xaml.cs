9512fae29d3547b9fe6b6b6079f26c18ce3196e9
JensVanGelder <jens.v.gelder@gmail.com>
2021-06-11 15:28:07 +0200

    Fixed Xamarin in solution

---
+++ ChefByStep/ChefByStep/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ChefByStep.Services;
+using ChefByStep.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChefByStep
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
