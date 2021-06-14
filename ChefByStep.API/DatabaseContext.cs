4e43399d82b2f20d0cc69ff46fc28b26b2ec02b0
JensVanGelder <jens.v.gelder@gmail.com>
2021-06-14 09:33:14 +0200

    Generic repo + start of DbContext

---
+++ ChefByStep.API/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChefByStep.API
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
