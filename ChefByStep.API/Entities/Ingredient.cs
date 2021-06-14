4e43c68d71cdcbe1454a543c3d510bc851ac5165
Diederik Van Gogh <doctor_music@hotmail.com>
2021-06-14 09:34:41 +0200

    Create Entities

---
+++ ChefByStep.API/Entities/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ChefByStep.API.Entities
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Quantity { get; set; }
+
+        public List<Recipe> Recipes { get; set; }
+
+
+
+
+    }
+}
---
+++ ChefByStep.API/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChefByStep.API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public int AccountId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Recipe> CreatedRecipe { get; set; }
+
+        public List<Recipe> FavoriteRecipes { get; set; }
+
+        public List<Recipe> CompletedRecipes { get; set; }
+
+        public List<RecipeRating> Rating { get; set; }
+
+
+
+
+
+    }
+}
