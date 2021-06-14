b81b5e1f76488ff85ebb779346ad8fa62657bcb9
Joyce A <joyce.alfvoet@gmail.com>
2021-06-14 10:31:03 +0200

    Created Models

---
+++ ChefByStep.ASP/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChefByStep.ASP.Models
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+
+        public Account Account { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Recipe> CreatedRecipes { get; set; }
+
+        public List<Recipe> FavouriteRecipes { get; set; }
+
+        public List<Recipe> CompletedRecipes { get; set; }
+
+        public List<RecipeRating> Ratings { get; set; }
+    }
+}
