d7dd057484f10ab661ed64c3fc9c233de3b81804
DotEmms <emma_danckaert@live.be>
2021-06-16 15:42:01 +0200

    trying to hide non-active steps

--- ChefByStep.ASP/wwwroot/js/site.js
+++ ChefByStep.ASP/wwwroot/js/site.js
@@ -2,27 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
 function toggleSteps() {
-    // get the steps
     var mySteps = document.getElementById('start-cooking');
 
-    // get the current value of the clock's display property
     var displaySetting = mySteps.style.display;
 
-    // also get the clock button, so we can change what it says
     var startCooking = document.getElementById('startCooking');
 
-    // now toggle the clock and the button text, depending on current state
     if (displaySetting == 'block') {
-        // clock is visible. hide it
         mySteps.style.display = 'none';
-        // change button text
         startCooking.innerHTML = 'Start Cooking';
     }
     else {
-        // clock is hidden. show it
         mySteps.style.display = 'block';
-        // change button text
         startCooking.innerHTML = 'Stop Cooking';
     }
 }
+
+function toggleStepByStep() {
+    var prev = $('.prev');
+    var next = $('.next');
+    var num = 0;
+
+    $('.li').hide();
+    $('.li').eq(0).show();
+
+    next.on('click', function () {
+        $('.li').hide();
+        num++;
+        if (num > 3) {
+            num = 0;
+        }
+        $('.li').eq(num).show();
+    });
+
+    prev.on('click', function () {
+        $('.li').hide();
+        num--;
+        if (num < 0) {
+            num = 3;
+        }
+        $('.li').eq(num).show();
+    });
+}
+
