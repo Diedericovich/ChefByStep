6342ae2019c087b42e724122c917025c67460a6b
volta76 <64655109+volta76@users.noreply.github.com>
2021-06-17 10:48:51 +0200

    Add User Repository

---
+++ ChefByStep/ChefByStep/Services/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChefByStep.Services.Repositories
+{
+    public class UserRepository
+    {
+        private const string baseUrl = "https://10.0.2.2:44350/api/Recipe";
+
+
+    }
+}
