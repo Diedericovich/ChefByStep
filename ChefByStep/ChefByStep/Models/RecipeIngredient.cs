d74b599e2c704c22ada818019ac33aeedc300bd0
volta76 <64655109+volta76@users.noreply.github.com>
2021-06-21 11:55:25 +0200

    Update Models

---
+++ ChefByStep/ChefByStep/Models/RecipeIngredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChefByStep.Models
+{
+    public class RecipeIngredient : BaseModel
+    {
+        public int IngredientId { get; set; }
+        public int RecipeId { get; set; }
+        public string Amount { get; set; }
+        public Recipe Recipe { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
