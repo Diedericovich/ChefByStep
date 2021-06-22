06511ba8af5759dbee3391ad32d22f02e5783e9a
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-06-22 11:09:29 +0200

    .

--- ChefByStep.ASP/Models/Step.cs
+++ ChefByStep.ASP/Models/Step.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ChefByStep.ASP.Models
+﻿namespace ChefByStep.ASP.Models
 {
     public class Step : BaseModel
     {
         public string Instruction { get; set; }
+
         public int DurationMin { get; set; }
+
         public bool IsDone { get; set; }
     }
 }
--- ChefByStep/ChefByStep/Models/Recipe.cs
+++ ChefByStep/ChefByStep/Models/Recipe.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Generic;
 
-    public class Recipe: BaseModel
+    public class Recipe : BaseModel
     {
         public int CategoryID { get; set; }
 
