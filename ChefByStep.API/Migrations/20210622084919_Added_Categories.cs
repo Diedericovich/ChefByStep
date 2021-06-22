ff43b11ee36a55a25488f7ebbd8164c2c2c3432f
JensVanGelder <jens.v.gelder@gmail.com>
2021-06-22 11:07:31 +0200

    API Added Category id to db and provided api controller Recipe/ByCategory/categoryId

---
+++ ChefByStep.API/Migrations/20210622084919_Added_Categories.cs
@@ -0,0 +1,444 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChefByStep.API.Migrations
+{
+    public partial class Added_Categories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Ingredients");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 4, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "CategoryId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 4, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 3, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 2, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 1, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 23,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 1, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 29,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 4, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 30,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 4, 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Quantity",
+                table: "Ingredients",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 23,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "CategoryId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 29,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 30,
+                columns: new[] { "CategoryId", "CreatedById" },
+                values: new object[] { 0, 1 });
+        }
+    }
+}
