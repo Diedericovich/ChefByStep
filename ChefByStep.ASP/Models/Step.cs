04dda126cd0a04ac21f5536371330b9a3f931a27
DotEmms <emma_danckaert@live.be>
2021-06-28 16:03:45 +0200

    Create is getting designed :D

--- ChefByStep.ASP/Models/Step.cs
+++ ChefByStep.ASP/Models/Step.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace ChefByStep.ASP.Models
 {
     public class Step : BaseModel
     {
+        [DisplayName("Instructions: ")]
         public string Instruction { get; set; }
         public int DurationMin { get; set; }
         public bool IsDone { get; set; }
--- ChefByStep.ASP/ViewModels/RecipeCreateViewModel.cs
+++ ChefByStep.ASP/ViewModels/RecipeCreateViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using ChefByStep.ASP.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +13,18 @@ namespace ChefByStep.ASP.ViewModels
         public int Id { get; set; }
         public int CreatedById { get; set; }
 
+        [DisplayName("Category: ")]
         public Category CategoryId { get; set; }
 
         public string Description { get; set; }
 
+        [DisplayName("Image Link: ")]
         public string ImageUrl { get; set; }
 
         public string Title { get; set; }
+        [DisplayName("Preparation Time (in minutes): ")]
         public int PrepTimeInMin { get; set; }
+        [DisplayName("Cooking Time (in minutes): ")]
         public int CookTimeInMin { get; set; }
 
         public List<Step> Steps { get; set; }
@@ -26,6 +32,5 @@ namespace ChefByStep.ASP.ViewModels
         public List<RecipeIngredient> Ingredients { get; set; }
         public ApiUser CreatedBy { get; set; }
 
-        public List<Step> Steps { get; set; }
     }
 }
